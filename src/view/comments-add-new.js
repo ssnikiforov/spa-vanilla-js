761b9c8837ea8e19a92807e895d158ecc8813c4b
ssnikiforov <stepan001@yandex.ru>
2022-05-12 18:37:20 +0300

    Remove duplicated lines of code

--- src/view/comments-add-new.js
+++ src/view/comments-add-new.js
@@ -1,6 +1,21 @@
 import { createElement } from '../render.js';
 
-const commentsAddNewTemplate = () =>
+const Emojis = {
+  SMILE: 'smile',
+  SLEEPING: 'sleeping',
+  PUKE: 'puke',
+  ANGRY: 'angry',
+};
+
+const addNewCommentsRowsTemplate = () => Object.values(Emojis).map(emoji => {
+  return `<input class="film-details__emoji-item visually-hidden"
+            name="comment-emoji" type="radio" id="emoji-${emoji}" value="${emoji}">
+      <label class="film-details__emoji-label" for="emoji-${emoji}">
+        <img src="./images/emoji/${emoji}.png" width="30" height="30" alt="${emoji}">
+      </label>`;
+}).join('');
+
+const addNewCommentsTemplate = () =>
   `<div class="film-details__new-comment">
     <div class="film-details__add-emoji-label"></div>
 
@@ -8,32 +23,12 @@ const commentsAddNewTemplate = () =>
       <textarea class="film-details__comment-input" placeholder="Select reaction below and write comment here" name="comment"></textarea>
     </label>
 
-    <div class="film-details__emoji-list">
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-smile" value="smile">
-      <label class="film-details__emoji-label" for="emoji-smile">
-        <img src="./images/emoji/smile.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-sleeping" value="sleeping">
-      <label class="film-details__emoji-label" for="emoji-sleeping">
-        <img src="./images/emoji/sleeping.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-puke" value="puke">
-      <label class="film-details__emoji-label" for="emoji-puke">
-        <img src="./images/emoji/puke.png" width="30" height="30" alt="emoji">
-      </label>
-
-      <input class="film-details__emoji-item visually-hidden" name="comment-emoji" type="radio" id="emoji-angry" value="angry">
-      <label class="film-details__emoji-label" for="emoji-angry">
-        <img src="./images/emoji/angry.png" width="30" height="30" alt="emoji">
-      </label>
-    </div>
+    <div class="film-details__emoji-list">${addNewCommentsRowsTemplate()}</div>
   </div>`;
 
 export default class CommentsAddNew {
   getTemplate () {
-    return commentsAddNewTemplate();
+    return addNewCommentsTemplate();
   }
 
   getElement () {
